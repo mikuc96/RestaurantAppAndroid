095e835d4f651a5734bb8950d6c15d001c6a6f32
mikuc <mikuc96@gmail.com>
2018-01-08 16:51:24 +0100

    Merge branch 'master' of https://bitbucket.org/kamilo116/restaurant
    # Conflicts:
    #	client/src/main/java/com/example/kamil/restaurant/OrderActivity.java

--- client/src/main/java/com/example/kamil/restaurant/StartActivity.java
+++ client/src/main/java/com/example/kamil/restaurant/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.kamil.restaurant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void menu(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void logowanie(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void rejestracja(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
