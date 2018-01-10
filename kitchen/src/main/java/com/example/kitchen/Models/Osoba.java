69be14020ef5d8fa4b6e0aecb690d3c69e51b36d
ks <krzsokmail@gmail.com>
2018-01-10 17:57:33 +0100

    modele do bazy, podlaczenie do firebase

---
+++ kitchen/src/main/java/com/example/kitchen/Models/Osoba.java
@@ -0,0 +1,71 @@
+package com.example.kitchen.Models;
+
+/**
+ * Created by dm on 10.01.18.
+ */
+
+public class Osoba {
+
+
+
+    private String login;
+    private Integer znizka;
+    private String email;
+    private String haslo;
+
+    private Integer id;
+
+    public Osoba() {
+
+    }
+
+    public Osoba(Integer id, String login, Integer znizka, String email, String haslo) {
+        this.id = id;
+        this.login = login;
+        this.znizka = znizka;
+        this.email = email;
+        this.haslo = haslo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getlogin() {
+        return login;
+    }
+
+    public void setlogin(String login) {
+        this.login = login;
+    }
+
+    public Integer getznizka() {
+        return znizka;
+    }
+
+    public void setznizka(Integer znizka) {
+        this.znizka = znizka;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public String gethaslo() {
+        return haslo;
+    }
+
+    public void sethaslo(String haslo) {
+        this.haslo = haslo;
+    }
+}
+        
+
